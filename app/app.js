78bb241b4777cda4ed1707fb8b829267aa65dfe9
kevmo <k.james.moore@gmail.com>
2016-04-30 15:13:51 -0400

    Handlebars view engine setup.

--- app/app.js
+++ app/app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,8 +13,23 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
+var partialsDir = __dirname + '/views/partials/';
+var hbs = exphbs.create({
+  defaultLayout: 'main',
+  partialsDir: [partialsDir]
+});
+
+app.set('views', __dirname + '/views');
+app.engine('handlebars', hbs.engine);
+//app.engine('hbs', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
