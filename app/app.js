3579fa3a29cef597d20290fa4d3ff09f09404b3b
kevmo <k.james.moore@gmail.com>
2016-04-30 15:40:46 -0400

    D3 working

--- app/app.js
+++ app/app.js
@@ -12,6 +12,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+// STATIC ASSETS: serving
+app.use(express.static('public'));
+
+
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
